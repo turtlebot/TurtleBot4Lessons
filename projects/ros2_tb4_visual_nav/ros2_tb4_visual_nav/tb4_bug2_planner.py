2126d9abc350d637887c41403223caaca59aaf4d
sumedhreddy90 <sumedhrk@umd.edu>
2022-06-23 11:53:18 -0400

    Bug Planner stubs

---
+++ projects/ros2_tb4_visual_nav/ros2_tb4_visual_nav/tb4_bug2_planner.py
@@ -0,0 +1,11 @@
+
+class tb4_pathplanner():
+        
+    def __init__(self):
+        self.Bug_2 = Bug2()
+
+class Bug2():
+
+    @staticmethod
+    def get_path():
+        return
--- projects/ros2_tb4_visual_nav/ros2_tb4_visual_nav/tb4_ride.py
+++ projects/ros2_tb4_visual_nav/ros2_tb4_visual_nav/tb4_ride.py
@@ -1,17 +1,23 @@
+from msilib.schema import SelfReg
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
-
+from geometry_msgs.msg import Twist
+from .tb4_bug2_planner import tb4_pathplanner
 
 class TurtleRide(Node):
 
     def __init__(self):
         super().__init__('turtle_planner_node')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        # Publishers
+        self.vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        timer_period = 0.2  # seconds
+        # Call backs
         self.timer = self.create_timer(timer_period, self.turtle_planner)
         self.i = 0
+        # Creating objects for each phase of the Robot Navugation
+        self.tb4_pathplanner = tb4_pathplanner()
 
     def turtle_planner(self):
         msg = String()
@@ -22,7 +28,7 @@ class TurtleRide(Node):
 
 
 def main(args=None):
-    
+
     rclpy.init(args=args)
     turtle_ride = TurtleRide()
     rclpy.spin(turtle_ride)
