4f41ea18e41075bac2b9311bdc8aa00666098274
sumedhreddy90 <sumedhrk@umd.edu>
2022-07-14 20:56:16 -0400

    changes to launch

--- projects/unit02_simulation_motion_planner/launch/tb4_perception.launch.py
+++ projects/unit02_simulation_motion_planner/launch/tb4_perception.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='ros2_tb4_visual_nav',
+            package='unit02_simulation_motion_planner',
             namespace='tb4_perception',
             executable='tb4_perception',
             name='turtlebot4_ride_perception'
