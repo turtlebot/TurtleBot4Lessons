fe355f4affd96f8ef41d8a031be1a34eb4bd6abc
sumedhreddy90 <sumedhrk@umd.edu>
2022-07-20 15:54:05 -0400

    resolving code readabilty as per second review

--- projects/unit02_simulation_motion_planner/unit02_simulation_motion_planner/extented_kalman_filter.py
+++ projects/unit02_simulation_motion_planner/unit02_simulation_motion_planner/extented_kalman_filter.py
@@ -8,9 +8,14 @@ from std_msgs.msg import Float64MultiArray
 
 class ExtentedKalmanFilter(Node):
     """
-    ExtentedKalmanFilter class inherits from (or is a subclass of) Node
-        Extended Kalman filter (EKF) is the nonlinear version of the Kalman filter
+    - The class TurtleGoal, attributes as a ROS Node that acts as a entrypoint
+        in the ROS system for communication especially for publishing
+        estimated state vector [x, y, yaw], which is an output from Extended Kalman filter
+    - Extended Kalman filter (EKF) is the nonlinear version of the Kalman filter
         which linearizes about an estimate of the current mean and covariance.
+        To understand better refer:
+        https://www.mathworks.com/help/driving/ug/extended-kalman-filters.html
+        https://medium.com/@serrano_223/extended-kalman-filters-for-dummies-4168c68e2117
 
     Attributes:
         Node: Is a class from rclpy.node.Node(node_name, *, context=None,
@@ -19,6 +24,20 @@ class ExtentedKalmanFilter(Node):
         allow_undeclared_parameters=False, automatically_declare_parameters_from_overrides=False)
         used to create a node, publish/ subscribe a node and access other ROS2 features
         More Information at: https://docs.ros2.org/latest/api/rclpy/api/node.html
+
+    Topics:
+    - Publishers:
+            - Topic name: /state_estimator
+                - Topic type: std_msgs.msg/Float64MultiArray
+                - Topic desciption: publish estimaed states of the extented kalman filter
+    - Subcribers:
+            - Topic name: /odom
+                - Topic type: nav_msgs.msg/Odometry
+                - Topic desciption: subscribe to odometry data of the turtlebot4
+            - Topic name: /cmd_vel
+                - Topic type: geometry_msgs.msg/Twist
+                - Topic desciption: To subscribe to current position and orientation of
+                     the turtlebot4
     """
 
     def __init__(self):
@@ -33,16 +52,61 @@ class ExtentedKalmanFilter(Node):
         # Publishers
         self.ekf_pub = self.create_publisher(Float64MultiArray,'/state_estimator',10)
 
+        # [A] matrix is 3x3 state matrix with number of states
+        # Describes how the system's [x, y, yaw] state changes from time t-1 to time t
+        # while no control input is issued
+        # here, A at t-1 is the identity matrix
+
         self.a_t_1 = np.array([[1.0,0,0],[0,1.0,0],[0,0,1.0]])
+
+        # At t-1, the estimated state vector in the global reference frame
+        # [x_t_1, y_t_1, yaw_t_1]
+
         self.state_vector_t_1 = np.array([0.0,0.0,0.0])
+
+        # At t-1, the control input vector (u) in the global reference frame
+        # [v,v,yaw]
+
         self.control_input_velocity = np.array([0.001,0.001,0.001])
+
+        # A [3*1] vector with noise appied while calculating estimated states at time t
+        # Process noise represents the idea/feature that the state of the system changes over time,
+        # but we do not know the exact details of when/how those changes occur, and
+        # thus we need to model them as a random process.
+        # the values choosen are by hit and trail
+
         self.process_noise_v_t_1 = np.array([0.088,0.088,0.026])
+
+        # h_t = measurement matrix
+        # This Matrix is used to convert the predicted state estimate at time equals to 1
+        # into predicted sensor measurements at time t.
+        # h_t is an identity matrix as estimated state directs directly to state measurements
+        # from the odometry data [x, y, yaw]
+
         self.h_t = np.array([[1.0,0,0],[0,1.0,0],[0,0,1.0]])
+
+        # Noise from sensor
         self.sensor_noise_w_t = np.array([0.06,0.06,0.03])
+
+        # deviation in time step at t-1 to t
         self.d_t = 0.002
+
+        # estimated yaw angle
         self.est_yaw_angle = 0.0
+
+        # State covariance matrix (P) at t-1 [3*3]
+        # It is an estimate of the accuracy of the state estimate at t=1
+        # which is calculated using the state transition matrix
+
         self.p_t_1 = np.array([[0.1,0,0],[0,0.1,0],[0,0,0.1]])
+
+        # State model noise covariance matrix Q_t
+        # To track changes in sensor measurements
         self.q_t = np.array([[1.0,0,0],[0,1.0,0],[0,0,1.0]])
+
+        # Sensor measurement noise covariance matrix R_t
+        # Values depends on accuracy of sensor measurements
+
         self.r_t = np.array([[1.0,0,0],[0, 1.0,0],[0,0,1.0]])
 
     def command_velocity(self, msg):
@@ -52,18 +116,20 @@ class ExtentedKalmanFilter(Node):
             msg:
                 Type: geometry_msgs/Twist.msg
                 Description: This expresses velocity in free space broken
-                into its linear and angular parts.
-                    1. Vector3  linear
-                    2. Vector3  angular
-        Forward velocity in the x direction robot's reference frame and
-        Angular velocity around the robot's z axis from geometry_msgs/Twist.msg
-        Output: velocity vector [linear_vel, linear_vel, angular_vel]
+                into its linear and angular parts
+
+                    - Vector3  linear
+                    - Vector3  angular
+
+        Forward linear velocity in the x direction along robot's reference frame and
+        Rotation around the robot's z axis from geometry_msgs/Twist.msg
+        Output: velocity vector [x, y, yaw]
         """
         # Forward velocity in the x direction robot's reference frame
         linear_vel = msg.linear.x
-        # Angular velocity around the robot's z axis
+        # Rotation around the robot's z axis from x-axis in counterclock wise
         angular_vel = msg.angular.z
-        # [linear_vel, linear_vel, angular_vel ]      
+        # [x, y, yaw ]
         self.control_input_velocity[0] = linear_vel
         self.control_input_velocity[1] = linear_vel
         self.control_input_velocity[2] = angular_vel
@@ -143,9 +209,14 @@ class ExtentedKalmanFilter(Node):
 
     def b_matrix(self,yaw,d_t):
         """
-        To calculate B-Matrix
+        - To calculate B-Matrix [3*3]
+        - [B] is the product of number of states and number of control inputs
+        - here control inputs [v,v,yaw] are linear velocity and yaw rate around
+            the z axis from the x-axis in the counterclockwise direction
+
         Args:
-             yaw: yaw angle -> Rotation about the z-axis
+             yaw: yaw angle -> Rotation about the z-axis from the x-axis 
+             in the counterclockwise direction
              d_t: differential time
 
         Return:
@@ -171,11 +242,12 @@ class ExtentedKalmanFilter(Node):
 
     def ekf(self, obs_vector):
         """
-        Extended Kalman Filter to return optimal states by calculating
-        current mean and covariance
+        Extended Kalman Filter Method
+        - Given a observation of the robot at given position,
+        this method estimates the robots position from prior inputs and our noise model
 
         Args:
-            obs_vector: input observation state vector from odometry_data 
+            obs_vector: input observation state vector from odometry_data
                         callback method [x, y, yaw]
 
         Output:
@@ -183,6 +255,7 @@ class ExtentedKalmanFilter(Node):
         """
         #Based on the state estimate at time t-1 and the control input applied
         # at time t-1, predict the state estimate at time t
+        # state estimate at t = [A](t-1) * x(t-1) + [B(t-1)] * u(t-1) + P_noise(t-1)
 
         state_estimate_t = self.a_t_1 @ (
             self.state_vector_t_1) + (
@@ -190,7 +263,9 @@ class ExtentedKalmanFilter(Node):
             self.control_input_velocity) + (
             self.process_noise_v_t_1)
 
-        #Estimate the state covariance using the prior covariance and noise.
+        # Estimate the state covariance using the prior covariance and noise.
+        # State covariance matrix (P(t)) = [A](t-1) * P(t-1) * [A](transp)(t-1) + Q_t
+        # here q_t State model noise covariance matrix
         p_t = self.a_t_1 @ self.p_t_1 @ self.a_t_1.T + (
             self.q_t)
 
@@ -200,12 +275,16 @@ class ExtentedKalmanFilter(Node):
             self.sensor_noise_w_t))
 
         # measurement residual covariance
+        # S(t) = H(t) * P(t) * H(T)^T + R(t)
+        # here H(t) = measurement matrix
+        # R(t) = Sensor measurement noise covariance matrix
         s_t = self.h_t @ p_t @ self.h_t.T + self.r_t
 
-        # near-optimal Kalman gain
+        # Near-optimal Kalman gain
+        # K_t = P(t) * H(t)^T * S(t)
         k_t = p_t @ self.h_t.T @ np.linalg.pinv(s_t)
 
-        # updated state estimate for time t
+        # Updated state estimate for time t
         state_estimate_t = state_estimate_t + (k_t @ measurement_residual_y_t)
 
         # Update the state covariance estimate for time t
